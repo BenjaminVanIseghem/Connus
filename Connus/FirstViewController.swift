c9722078bb9847f38aba582889734c15e4db63ef
Benjamin Van Iseghem <benjamin.vaniseghem.w3891@student.hogent.be>
2019-04-26 20:10:15 +0200

    Initial Commit

---
+++ Connus/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Connus
+//
+//  Created by Benjamin Van Iseghem on 26/04/2019.
+//  Copyright © 2019 Connus. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Connus/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Connus
+//
+//  Created by Benjamin Van Iseghem on 26/04/2019.
+//  Copyright © 2019 Connus. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
